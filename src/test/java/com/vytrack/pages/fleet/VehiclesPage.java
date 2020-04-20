79df47322787172f0f476647a3d0331a728d6cc6
Gunel Khantamirova <gun040el@gmail.com>
2020-04-20 12:39:38 -0400

    first commit

---
+++ src/test/java/com/vytrack/pages/fleet/VehiclesPage.java
@@ -0,0 +1,30 @@
+package com.vytrack.pages.fleet;
+
+
+ //import com.automation.pages.AbstractPageBase;
+import com.vytrack.pages.AbstractPageBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class VehiclesPage extends AbstractPageBase {
+
+    @FindBy(partialLinkText =  "Create Car")
+    private WebElement createCar;
+
+
+    public void clickCreateCar(){
+        WebDriverWait wait = new WebDriverWait(driver , 10);
+        wait.until(ExpectedConditions.elementToBeClickable(createCar)).click();
+
+    }
+
+
+
+
+
+
+
+
+}
