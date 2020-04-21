f6aaac1667739f38613a18eeb3edd666e30c78a4
Gunel Khantamirova <gun040el@gmail.com>
2020-04-21 00:17:25 -0400

    vytrack

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -1,30 +1,38 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class LoginStepDefinitions {
 
-
-    @Given("user is on the landing page")
-    public void user_is_on_the_landing_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    LoginPage loginPage = new LoginPage();
+​
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+        System.out.println("Open login page");
+​
+        String URL = ConfigurationReader.getProperty("qa3");
+        Driver.getDriver().get(URL);
     }
-
-
-  //  Some other steps were also undefined:
-
+​
     @When("user logs in as a sales manager")
     public void user_logs_in_as_a_sales_manager() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Login as sales manager");
+​
+        loginPage.login();
     }
+​
     @Then("user should verify that title is a Dashboard")
     public void user_should_verify_that_title_is_a_Dashboard() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Verify that title is a Dashboard");
+​
+        Assert.assertEquals("Dashboard", Driver.getDriver().getTitle());
+        Driver.closeDriver();
     }
 
 
