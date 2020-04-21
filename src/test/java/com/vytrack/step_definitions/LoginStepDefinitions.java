259dd1a375afcbe7a714fa84f51eeceef57b3f83
Gunel Khantamirova <gun040el@gmail.com>
2020-04-21 00:28:26 -0400

    vytrack

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -9,31 +9,31 @@ import io.cucumber.java.en.When;
 import org.junit.Assert;
 
 public class LoginStepDefinitions {
-
     LoginPage loginPage = new LoginPage();
-​
-    @Given("user is on the login page")
-    public void user_is_on_the_login_page() {
+
+    @Given("user is on the landing page")
+    public void user_is_on_the_landing_page() {
         System.out.println("Open login page");
-​
         String URL = ConfigurationReader.getProperty("qa3");
         Driver.getDriver().get(URL);
     }
-​
+
+
+
+
     @When("user logs in as a sales manager")
     public void user_logs_in_as_a_sales_manager() {
-        System.out.println("Login as sales manager");
-​
         loginPage.login();
     }
-​
+
+
     @Then("user should verify that title is a Dashboard")
     public void user_should_verify_that_title_is_a_Dashboard() {
         System.out.println("Verify that title is a Dashboard");
-​
         Assert.assertEquals("Dashboard", Driver.getDriver().getTitle());
         Driver.closeDriver();
     }
+
 
 
 }
