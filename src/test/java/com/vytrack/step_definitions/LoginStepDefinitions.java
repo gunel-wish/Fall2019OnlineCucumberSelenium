53edb1233f178c60e0e156235dda924937f97f04
Gunel Khantamirova <gun040el@gmail.com>
2020-04-21 01:03:25 -0400

    vytrack

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -14,22 +14,30 @@ public class LoginStepDefinitions {
     @Given("user is on the landing page")
     public void user_is_on_the_landing_page() {
         System.out.println("Open login page");
+
         String URL = ConfigurationReader.getProperty("qa3");
         Driver.getDriver().get(URL);
     }
 
 
-
-
     @When("user logs in as a sales manager")
     public void user_logs_in_as_a_sales_manager() {
-        loginPage.login();
+        System.out.println("Login as sales manager");
+        loginPage.login("salesmanager110","UserUser123");
+    }
+
+
+    @When("user logs in as a store manager")
+    public void user_logs_in_as_a_store_manager() {
+       loginPage.login("storemanager85" , "UserUser123");
+
     }
 
 
     @Then("user should verify that title is a Dashboard")
     public void user_should_verify_that_title_is_a_Dashboard() {
         System.out.println("Verify that title is a Dashboard");
+
         Assert.assertEquals("Dashboard", Driver.getDriver().getTitle());
         Driver.closeDriver();
     }
