e539a9bae54fa7daf22adcbc40c7a452a2d98a53
Gunel Khantamirova <gun040el@gmail.com>
2020-04-24 02:48:45 -0400

    vytrack

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -27,13 +27,13 @@ public class LoginStepDefinitions {
     @When("user logs in as a sales manager")
     public void user_logs_in_as_a_sales_manager() {
         System.out.println("Login as sales manager");
-      loginPage.login("salesmanager110","UserUser123");
+      loginPage.login("salesmanager110", "UserUser123");
     }
 
 
     @When("user logs in as a store manager")
     public void user_logs_in_as_a_store_manager() {
-        loginPage.login("storemager85" , "UserUser123");
+        loginPage.login("storemanager85" , "UserUser123");
     }
 
 
@@ -43,7 +43,7 @@ public class LoginStepDefinitions {
         BrowserUtilities.waitForPageToLoad(2);
         System.out.println("Verify that title is a Dashboard");
         Assert.assertEquals("Dashboard",Driver.getDriver().getTitle());
-        Driver.closeDriver();
+
 
 
     }
@@ -51,7 +51,7 @@ public class LoginStepDefinitions {
     @When("user logs in as a driver")
     public void user_logs_in_as_a_driver() {
         System.out.println("Login as a driver");
-        loginPage.login("user19","userUser123");
+        loginPage.login("user19", "UserUser123");
     }
 
 
