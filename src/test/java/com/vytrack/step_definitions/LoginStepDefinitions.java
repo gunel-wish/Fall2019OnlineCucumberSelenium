68cdb72f9f7a441582d0333d0808eef26aa89cab
Gunel Khantamirova <gun040el@gmail.com>
2020-04-28 01:12:02 -0400

    vytrack

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -19,7 +19,7 @@ public class LoginStepDefinitions {
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
         System.out.println("Open login page");
-        String URL = ConfigurationReader.getProperty("qa3");
+        String URL = ConfigurationReader.getProperty("qa1");
         Driver.getDriver().get(URL);
 
     }
@@ -75,8 +75,21 @@ public class LoginStepDefinitions {
     public void user_name_should_be(String string) {
        Assert.assertEquals(string , loginPage.getCurrentUserName());
 
+    }
+
+
+    @When("user logs in as {string}")
+    public void user_logs_in_as(String string) {
+      loginPage.login();
 
     }
+
+    @Then("user verifies that page title is {string}")
+    public void user_verifies_that_page_title_is(String string) {
+        System.out.println("Verify that page title is: " + string);
+        Assert.assertEquals(string, Driver.getDriver().getTitle());
+    }
+
 
 
 
