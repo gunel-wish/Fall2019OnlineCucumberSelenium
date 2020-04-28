29be65dc87d81bf1eaafa42175254949e1f9119a
Gunel Khantamirova <gun040el@gmail.com>
2020-04-27 22:35:52 -0400

    vytrack

--- src/test/java/com/vytrack/step_definitions/CreateCarStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/CreateCarStepDefinitions.java
@@ -2,6 +2,9 @@ package com.vytrack.step_definitions;
 
 import com.vytrack.pages.fleet.VehiclesPage;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.it.Ma;
+import org.apache.commons.io.output.ProxyWriter;
+import org.junit.Assert;
 
 import java.util.List;
 import java.util.Map;
@@ -17,19 +20,52 @@ public class CreateCarStepDefinitions {
 
     }
 
-    // Then user creates a car which following info:
-    //     | License Plate | Driver   |Location     | Model Year | Color  |
-    //     | SDET          | Pro Racer| Rome ,Italy |     2020   |red     |
-    @Then("user creates a car which following info:")
-    public void user_creates_a_car_which_following_info(List<Map<String ,String>>  dataTable) {
-        System.out.println(dataTable);
-    vehiclesPage.setLicencePlateInput(dataTable.get(0).get("License Plate"));
-     vehiclesPage.setLicencePlateInput(dataTable.get(0).get("Driver"));
-     vehiclesPage.setLocationInput(dataTable.get(0).get("Location"));
-     vehiclesPage.setModelYear(dataTable.get(0).get("Model Year"));
-     vehiclesPage.setColor(dataTable.get(0).get("Color"));
 
+    //    Then user creates a car with following info:
+//            | License Plate | Driver    | Location    | Model Year | Color |
+//            | SDET          | Pro Racer | Rome, Italy | 2020       | Red   | 0 row
+
+    @Then("user creates a car which following info:")
+    public void user_creates_a_car_which_following_info(List<Map<String, String>> dataTable) {
+        System.out.println(dataTable);
+        for (Map<String, String> row : dataTable) {
+            vehiclesPage.setLicencePlateInput(row.get("License Plate"));
+
+            vehiclesPage.setDriverInput(row.get("Driver"));
+
+            vehiclesPage.setLocationInput(row.get("Location"));
+
+            vehiclesPage.setModelYear(row.get("Model Year"));
+
+            vehiclesPage.setColor(row.get("Color"));
+            //click to save new car
+            vehiclesPage.clickOnSaveAndClose();
+        }
     }
+
+    @Then("user verifies that car info is displayed:")
+    public void user_verifies_that_car_info_is_displayed(List<Map<String, String>> dataTable) {
+        for (Map<String , String> row : dataTable){
+      Assert.assertEquals(row.get("License Plate"),vehiclesPage.getCarGeneralInfo("License Plate"));
+      Assert.assertEquals(row.get("Driver"),vehiclesPage.getCarGeneralInfo("Driver"));
+      Assert.assertEquals(row.get("Location"),vehiclesPage.getCarGeneralInfo("Location"));
+      Assert.assertEquals(row.get("Model Year"),vehiclesPage.getCarGeneralInfo("Model Year"));
+      Assert.assertEquals(row.get("Color"),vehiclesPage.getCarGeneralInfo("Color"));
+        }
+    }
+
+//            | License Plate | Driver    | Location    | Model Year | Color |
+
+
+
+
+
+
+
+
+
+
+
 
 
 
